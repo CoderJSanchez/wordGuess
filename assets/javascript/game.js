5582c3bae4354f4e0f8e5495e909276d1f05479c
julioSanchez <juliosanchez@Julios-MBP.local>
2018-08-27 18:38:51 -0500

    pushed correct guess to an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,9 +27,12 @@ for(let j = 0; j < randomWord.length; j++ ){
 document.onkeyup = function(event){
     var chosenLetter = event.key;
     console.log(randomWord.indexOf(chosenLetter));
-    if(chosenLetter.indexOf(randomWord) > -1){
-        console.log(true);
+    //this "if" checks of the chosenLetter exist in an index position of randomWord
+    //if it is not then it will console -1 because that is how JS shows something is not in an array
+    if(randomWord.indexOf(chosenLetter) > -1){
+        showLetter.push(chosenLetter);
     }
+    console.log(showLetter);
 }
 
 
@@ -37,44 +40,3 @@ document.onkeyup = function(event){
 //document.getElementById('test').innerHTML = showLetter;
 console.log(randomWord);
 console.log(dashArray);
-
-
-//*********************************************************************** */
-
-// //creates a random number
-// let randomWord = Math.floor(Math.random() * wordList.length);
-
-// //takes the random number and applies it to wordList index
-// let choice = wordList[randomWord];
-
-// //creates an empty array to hold the underscores
-// const underScoreArray = [];
-
-
-
-// //function to generate underscores
-// const underScoreGenerator = () => {
-//     for(let i = 0; i < choice.length; i++){
-//         underScoreArray.push(" _ ");
-//     }
-//     return underScoreArray;
-//     console.log(underScoreArray);
-// }
-// //show the chosen word (just for my purposes right now)
-// console.log(choice);
-
-// //show the chosen word as dashes
-// console.log(underScoreGenerator());
-
-
-// // Get the input from the user
-
-// document.addEventListener("keyup", (letter) => {
-//     let letterGuessed = letter.key;
-//     console.log(letterGuessed);
-
-//     if(letterGuessed === underScoreArray){
-//         underScoreArray.push(letterGuessed);
-//     }
-//     console.log(underScoreArray);
-// });
