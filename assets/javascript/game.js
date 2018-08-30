3432fa73921191d292b2a10eea86eeadacbaf868
julioSanchez <juliosanchez@Julios-MacBook-Pro.local>
2018-08-30 17:06:50 -0500

    basic functionality is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@
 
 //Array of words to be selected
 var wordList = ['computer', 'harddrive', 'mouse', 'keyboard', 'screen', 'memory', 'printer', 'javascript', 'python', 'array', 'loop', 'variable'];
-var remainingGuess = 3;
+var remainingGuess = 9;
 var dashArray = [];
 var wrongLetter = [];
 
@@ -15,34 +15,52 @@ var wrongLetter = [];
 //creates a random word from the wordList array
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 console.log(randomWord);   
-
-for(let i = 0; i < randomWord.length; i++){
-   dashArray[i] = ' _ ';
-   document.getElementById("answerSpan").innerHTML = dashArray.join(' ');
-   //console.log(dashArray[i]);  why does this look different in the console?
+//this function will geneate a new random word when called
+function createNewWord(){
+    randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(randomWord); 
 }
+//this function will create underscores for each new word that was called
+function makeNewDashes(){
+    for(let i = 0; i < randomWord.length; i++){
+    dashArray[i] = ' _ ';
+    document.getElementById("answerSpan").innerHTML = dashArray.join(' ');
+    //console.log(dashArray[i]);  why does this look different in the console?
+    }
+}
+makeNewDashes();
+
+
+
 
 //record the letter by user
 document.onkeyup = (event) => {
     var playerGuess = event.key;
-    remainingGuess--;
 
-    if(remainingGuess === 0){
-        wrongLetter = [];
-        remainingGuess = 3;
-    }
-    //if the letter guessed by the player is = to -1, which means it is not in the word, then the letter is pushed to the wrongLetter Array.
-    if(randomWord.indexOf(playerGuess) == -1){
-        wrongLetter.push(playerGuess);
-        console.log(' this is wrongLetterArry: ' +  wrongLetter);
-    }
-    
     //this will check if userguess matches an index of randomWord.  If so, it will then set the matching dashArray index to the letter guessed.
     for(let i = 0; i < randomWord.length; i++){
         if(randomWord[i] === playerGuess){
             dashArray[i] = playerGuess;    
         }
      }
+     //this if blcok checks if the letter chosen is not in the array.  It then pushes the letter top the wrongLetter array and decrements the remaining guesses
+     if(randomWord.indexOf(playerGuess) == -1){
+        wrongLetter.push(playerGuess);
+        remainingGuess--;
+        console.log(' this is wrongLetterArry: ' +  wrongLetter);
+    }
+
+    
+        //if all guesses are used up then the following block executes.  
+        if(remainingGuess < 1){
+        remainingGuess = 9;
+        alert(`answer was ${randomWord}`);
+        createNewWord();
+        wrongLetter = [];
+        makeNewDashes();
+        }
+    
+    
      console.log(dashArray);
      //DOM manipulation
      document.getElementById("answerSpan").innerHTML = dashArray.join('');   
