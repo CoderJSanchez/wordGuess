8ad40f43752ea7c9440b0f4f5740a151a8800ff1
julioSanchez <juliosanchez@Julios-MBP.local>
2018-08-30 03:32:55 -0500

    working on the reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,39 +7,49 @@
 
 //Array of words to be selected
 var wordList = ['computer', 'harddrive', 'mouse', 'keyboard', 'screen', 'memory', 'printer', 'javascript', 'python', 'array', 'loop', 'variable'];
-var remainingGuess = 15;
+var remainingGuess = 3;
 var dashArray = [];
-var showLetter = [];
 var wrongLetter = [];
 
 
 //creates a random word from the wordList array
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-console.log(randomWord + 'this is conosled from randomWord in line 18');   
+console.log(randomWord);   
 
 for(let i = 0; i < randomWord.length; i++){
    dashArray[i] = ' _ ';
+   document.getElementById("answerSpan").innerHTML = dashArray.join(' ');
+   //console.log(dashArray[i]);  why does this look different in the console?
 }
-document.getElementById("answerSpan").innerHTML = dashArray;
 
 //record the letter by user
 document.onkeyup = (event) => {
     var playerGuess = event.key;
     remainingGuess--;
-    console.log(remainingGuess);
+
+    if(remainingGuess === 0){
+        wrongLetter = [];
+        remainingGuess = 3;
+    }
+    //if the letter guessed by the player is = to -1, which means it is not in the word, then the letter is pushed to the wrongLetter Array.
+    if(randomWord.indexOf(playerGuess) == -1){
+        wrongLetter.push(playerGuess);
+        console.log(' this is wrongLetterArry: ' +  wrongLetter);
+    }
+    
+    //this will check if userguess matches an index of randomWord.  If so, it will then set the matching dashArray index to the letter guessed.
     for(let i = 0; i < randomWord.length; i++){
         if(randomWord[i] === playerGuess){
             dashArray[i] = playerGuess;    
-        }else{
-            if(randomWord[i]!==playerGuess){
-                wrongLetter.push(playerGuess);        
-            }          
         }
      }
-     console.log(`This is dashArray outside the for loop: ${dashArray}`);
-     document.getElementById("answerSpan").innerHTML = dashArray;
-     
-    if(playerGuess !== randomWord){
-        wrongLetter.push(playerGuess);
-    }
+     console.log(dashArray);
+     //DOM manipulation
+     document.getElementById("answerSpan").innerHTML = dashArray.join('');   
+     document.getElementById("wrongLetterSpan").innerHTML = wrongLetter;
+     document.getElementById("guessesLeftSpan").innerHTML = remainingGuess; //?? Why does this have to be inside and outside?
+
+
 }   //<-- closing brace for onkeyup event
+document.getElementById("guessesLeftSpan").innerHTML = remainingGuess;
+
